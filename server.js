d860c48eca104528b5ed8c56a469b2ed255c3c58
Ralph Recinto <pologcooks@gmail.com>
2023-08-22 14:33:02 -0700

    Fix user route URL

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/social-network-api', {
   useUnifiedTopology: true
 });
 
