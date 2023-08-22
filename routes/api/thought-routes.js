fc86a30ea30827e80129497d89ae2a6e0ab72975
Ralph Recinto <pologcooks@gmail.com>
2023-08-22 15:21:58 -0700

    Add other thought routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -50,4 +50,70 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.put('/:id', async (req, res) => {
+    try {
+        const thought = await Thought.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!thought) {
+            res.status(404).json({ message: 'No thought found with this id!' });
+            return;
+        }
+        res.json(thought);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const thought = await Thought.findByIdAndDelete(req.params.id);
+
+        if (!thought) {
+            res.status(404).json({ message: 'No thought found with this id!' });
+            return;
+        }
+
+        res.status(201).json(thought);
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred' });
+    }
+});
+
+
+router.post('/:id/reactions', async (req, res) => {
+    try {
+        const thought = await Thought.findById(req.params.id);
+
+        if (!thought) {
+            return res.status(404).json({ error: 'Thought not found' });
+        }
+
+        const newReaction = {
+            reactionBody: req.body.reactionBody,
+            username: req.body.username
+        };
+
+        thought.reactions.push(newReaction);
+        await thought.save();
+        res.status(201).json(thought);
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred' });
+    }
+});
+
+router.delete('/:id/reactions/:reactionId', async (req, res) => {
+    try {
+        const thought = await Thought.findById(req.params.id);
+
+        if (!thought) {
+            return res.status(404).json({ error: 'Thought not found' });
+        }
+
+        thought.reactions = thought.reactions.filter(reaction => reaction.reactionId.toString() !== req.params.reactionId);
+        await thought.save();
+        res.status(200).json(thought);
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred' });
+    }
+});
+
 module.exports = router;
